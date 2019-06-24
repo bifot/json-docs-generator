ae8153651ce4e096a64baa8be57ca5448bcea935
bifot <bifot@bifot.ru>
2019-06-24 11:57:14 +0300

    do not format number/string values

--- src/utils/formatResponse.js
+++ src/utils/formatResponse.js
@@ -3,10 +3,16 @@ const normalize = response => Object.entries(response).reduce((object, [key, val
   [key]: typeof value === 'function' ? typeof value() : value,
 }), {});
 
-module.exports = response => JSON.stringify(
-  Array.isArray(response) ? response.map(normalize) : normalize(response),
-  null,
-  2,
-)
-  .replace(/".*":/g, match => `${match.substr(1, match.length - 3)}:`)
-  .replace(/"/g, '\'');
+module.exports = (response) => {
+  if (typeof response === 'number' || typeof response === 'string') {
+    return response;
+  }
+
+  return JSON.stringify(
+    Array.isArray(response) ? response.map(normalize) : normalize(response),
+    null,
+    2,
+  )
+    .replace(/".*":/g, match => `${match.substr(1, match.length - 3)}:`)
+    .replace(/"/g, '\'');
+};
